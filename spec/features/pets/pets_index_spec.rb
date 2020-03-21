7c6c73e526e4c7e763ecf1aee296668363e9c2c3
Andrew Reid <areid.sj@gmail.com>
2020-03-21 07:47:17 -0600

    Refactor pets index test to use before block

--- spec/features/pets/pets_index_spec.rb
+++ spec/features/pets/pets_index_spec.rb
@@ -1,36 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "PETS index page - A user", type: :feature do
-  it "can see the name and info of each pet in the system" do
-    shelter_1 = Shelter.create( name: "Henry Porter's Puppies",
+  before(:each) do
+    @shelter_1 = Shelter.create( name: "Henry Porter's Puppies",
                                 address: "1315 Monaco Parkway",
                                 city: "Denver",
                                 state: "CO",
                                 zip: "80220"
                               )
-    pet_1 = Pet.create( name: "Henry",
+    @pet_1 = Pet.create( name: "Henry",
                         age: 4,
                         sex: "Male",
-                        shelter_id: shelter_1.id,
+                        shelter_id: @shelter_1.id,
                         image_path: 'hp.jpg',
                         description: 'Cute',
                         adoptable_status: 'Adoptable')
-    pet_2 = Pet.create( name: "Liza Bear",
+    @pet_2 = Pet.create( name: "Liza Bear",
                         age: 16,
                         sex: "Female",
-                        shelter_id: shelter_1.id,
+                        shelter_id: @shelter_1.id,
                         image_path: 'hp2.jpg',
                         description: 'Cute',
                         adoptable_status: 'Adoptable')
+  end
+  it "can see the name and info of each pet in the system" do
 
     visit "/pets"
 
-    expect(page).to have_content(pet_1.name)
-    expect(page).to have_content(pet_2.name)
-    expect(page).to have_content(pet_1.age)
-    expect(page).to have_content(pet_2.age)
-    expect(page).to have_content(pet_1.sex)
-    expect(page).to have_content(pet_2.sex)
+    expect(page).to have_content(@pet_1.name)
+    expect(page).to have_content(@pet_2.name)
+    expect(page).to have_content(@pet_1.age)
+    expect(page).to have_content(@pet_2.age)
+    expect(page).to have_content(@pet_1.sex)
+    expect(page).to have_content(@pet_2.sex)
     expect(page).to have_css("img[src='/assets/hp-606612a36d3cc16d901e74616fbd73a568030910d171797aa44123d55a9bfa70.jpg']")
     expect(page).to have_css("img[src='/assets/hp2-d54ec5938e641f10459be7bdba8fbb7fed849ec44ba2d1ed8568773d69bd164d.jpg']")
   end
