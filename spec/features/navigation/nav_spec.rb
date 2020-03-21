fc7f14abafc47df8a8a57aaf0c84d84cfb3237dd
Andrew Reid <areid.sj@gmail.com>
2020-03-21 08:39:20 -0600

    Refactor specs to move nav test to its own folder

--- spec/features/navigation/nav_spec.rb
+++ spec/features/navigation/nav_spec.rb
@@ -23,18 +23,21 @@ RSpec.describe "PETS show page - A user", type: :feature do
                         adoptable_status: 'Pending Adoption',
                         description: "Newfie drool!")
   end
-
-  it "can update a pet" do
+    it "can nav to pets index" do
     visit "/pets/#{@pet_1.id}/edit"
+    within("nav") do
+      click_link "All Pets"
+    end
 
-    fill_in(:sex, :with => "Female")
-    fill_in(:description, :with => "You won't find any cuter!")
-    fill_in(:image_path, :with => "hp2.jpg")
-    click_button "Update Pet"
+    expect(page).to have_current_path("/pets")
+  end
 
-    expect(page).to have_current_path("/pets/#{@pet_1[:id]}")
-    expect(page).to have_content("Female")
-    expect(page).to have_content("You won't find any cuter!")
-    expect(page).to have_css("img[src='/assets/hp2-d54ec5938e641f10459be7bdba8fbb7fed849ec44ba2d1ed8568773d69bd164d.jpg']")
+  it "can nav to shelters index" do
+    visit "/pets/#{@pet_1.id}/edit"
+    within("nav") do
+      click_link "All Shelters"
+    end
+
+    expect(page).to have_current_path("/shelters")
   end
 end
