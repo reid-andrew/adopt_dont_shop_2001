146fc7d36059f350aa61367dc8a06e8860cddc77
Andrew Reid <areid.sj@gmail.com>
2020-03-21 08:35:59 -0600

    Write and pass user story 19 and 20 test for pet edit page

--- spec/features/pets/pets_edit_spec.rb
+++ spec/features/pets/pets_edit_spec.rb
@@ -37,4 +37,22 @@ RSpec.describe "PETS show page - A user", type: :feature do
     expect(page).to have_content("You won't find any cuter!")
     expect(page).to have_css("img[src='/assets/hp2-d54ec5938e641f10459be7bdba8fbb7fed849ec44ba2d1ed8568773d69bd164d.jpg']")
   end
+
+  it "can nav to pets index" do
+    visit "/pets/#{@pet_1.id}/edit"
+    within("nav") do
+      click_link "All Pets"
+    end
+
+    expect(page).to have_current_path("/pets")
+  end
+
+  it "can nav to shelters index" do
+    visit "/pets/#{@pet_1.id}/edit"
+    within("nav") do
+      click_link "All Shelters"
+    end
+
+    expect(page).to have_current_path("/shelters")
+  end
 end
