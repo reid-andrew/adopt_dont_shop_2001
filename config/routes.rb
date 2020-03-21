4d044f25c001f797a19aa998bd796dda3aef3a36
Andrew Reid <areid.sj@gmail.com>
2020-03-21 10:54:03 -0600

    Refactor shelter delete links and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/shelters/:id', to: 'shelters#show'
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
-  get '/shelters/:id/delete', to: 'shelters#destroy'
+  delete '/shelters/:id', to: 'shelters#destroy'
   get '/shelters/:id/pets', to: 'showpets#index'
   get '/shelters/:id/pets/new', to: 'pets#new'
   post '/shelters/:id/pets', to: 'pets#create'
