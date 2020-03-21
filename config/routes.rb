6e90831b0f88cee76c70bd71c7de956040e8fed3
Andrew Reid <areid.sj@gmail.com>
2020-03-21 10:03:09 -0600

    Refactor shelter create form and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
-  post '/shelters/new_shelter', to: 'shelters#create'
+  post '/shelters', to: 'shelters#create'
   get '/shelters/:id', to: 'shelters#show'
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id/edit', to: 'shelters#update'
