219d79ba5c2b06bba29ed41321f6781a4ea9924b
Andrew Reid <areid.sj@gmail.com>
2020-03-22 14:34:28 -0600

    Add additional test coverage to pets edit spec

--- spec/features/pets/pets_edit_spec.rb
+++ spec/features/pets/pets_edit_spec.rb
@@ -36,5 +36,17 @@ RSpec.describe "PETS show page - A user", type: :feature do
     expect(page).to have_content("Female")
     expect(page).to have_content("You won't find any cuter!")
     expect(page).to have_css("img[src='/assets/hp2-d54ec5938e641f10459be7bdba8fbb7fed849ec44ba2d1ed8568773d69bd164d.jpg']")
+
+    visit "/pets/#{@pet_1.id}/edit"
+
+    fill_in(:sex, :with => "Male")
+    fill_in(:age, :with => 99)
+    fill_in(:description, :with => "Likes to play fetch")
+    click_button "Update Pet"
+
+    expect(page).to have_current_path("/pets/#{@pet_1[:id]}")
+    expect(page).to have_content("Male")
+    expect(page).to have_content(99)
+    expect(page).to have_content("Likes to play fetch")
   end
 end
